e748c738ff3bfcebd49c89e1d02e095e584e77b8
wilmeragudelo <105666923+wilmeragudelo@users.noreply.github.com>
2026-01-15 06:27:52 -0500

    stados derivados de otros etados

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import { TodoItem } from './TodoItem';
 const defaultTodos = [
   { text: 'Llevar la ropa para entrenar', completed: true},
   { text: 'Tomar el Cuerso de Intro a React.js', completed: false},
-  { text: 'Precticar Guitarra', completed: true},
-  { text: 'Estudiar React', completed: false},
-  { text: 'Pasar por la caja menor', completed: false},
+  { text: 'Estudiar React', completed: true},
+  { text: 'Pasar por la caja menor', completed: true},
+  { text: 'Ir a la reunión de IA', completed: false},
+
 ];
 
 
@@ -20,21 +21,31 @@ function App() {
   console.log('los usuarios buscan: ' + searchValue);
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+  // const totalTodos = todos.filter(todo  => todo.text).length;
 
-  // const totalTodos = todos.length
+  const serchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLocaleLowerCase();
+      const searchText = searchValue.toLowerCase();
 
+      return todoText.includes(searchText);
+    }
+  )
 
   return (
     <>
       <TodoCounter 
         completed={completedTodos} 
-        total={25}/>
+        total={totalTodos}
+      />
       <TodoSearch 
         searchValue={searchValue}
         setSearchValue={setSearchValue}
+
       />
       <TodoList>
-        {defaultTodos.map( todo => (
+        {serchedTodos.map( todo => (
           <TodoItem 
           key={todo.text} 
           text={todo.text}
