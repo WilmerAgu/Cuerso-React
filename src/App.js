11ad1d1177ea3c93301b1856a43615ea2cf4de42
wilmeragudelo <105666923+wilmeragudelo@users.noreply.github.com>
2026-01-19 07:56:21 -0500

    se crean las funciones para el manejo de estados, compled y delete

--- src/App.js
+++ src/App.js
@@ -33,6 +33,24 @@ function App() {
     }
   )
 
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos [todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = todos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+  
   return (
     <>
       <TodoCounter 
@@ -47,9 +65,12 @@ function App() {
       <TodoList>
         {serchedTodos.map( todo => (
           <TodoItem 
-          key={todo.text} 
-          text={todo.text}
-          completed={todo.completed} /> 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed} 
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          /> 
         ))}
       </TodoList> 
       <CreateTodoButton />
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,6 +1,15 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed}) {
+
+    if (total > 0 && completed === total){
+        return (
+            <h1 className="TodoCounter">
+                ¡Muy bien! Has completado todas las tareas
+            </h1>
+        )
+    };
+
     return (
         <h1 className="TodoCounter"> 
             Has completado <span>{completed}</span> de <span>{total}</span> TODOs
